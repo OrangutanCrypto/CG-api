9eda1e265e527d560fccef78d0874447e656e9d8
OrangutanCrypto <orangcoin@gmail.com>
2022-02-25 00:21:38 -0800

    second commit

---
+++ node.js
@@ -0,0 +1,27 @@
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    tiers: '1',
+    tags: 'dex,exchange',
+    orderBy: 'marketCap',
+    orderDirection: 'desc',
+    limit: '50',
+    offset: '0'
+  },
+  headers: {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '0832adf148msh8c8d2d05b5251b8p16425ajsn8dcd8d5d5b4e'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+console.log(data)
